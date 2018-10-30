5abc16541f0184d899aa78bf302521e45cc61a21
mmacdonald1 <melmacd94@gmail.com>
2018-10-30 15:27:39 -0400

    index and show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: [:show]
+  def index
+    @users = User.all
+  end
+  private
+  def find_user
+    @user = User.find(params[:id])
+  end
 end
